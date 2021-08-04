478d366ce0c20a5e44e1c628f4c7fbff954ae1c5
Leif Bersamina <leifbersamina@gmail.com>
2021-08-04 20:53:48 +0800

    add get_area method for both shapes

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -1,5 +1,9 @@
 class Circle < Shape
-    has_one :rad
+    has_one :rad, dependent: :destroy
 
     accepts_nested_attributes_for :rad
+
+    def get_area 
+      self.update(area: Math::PI * (self.rad.value**2)) 
+    end
 end
--- app/models/rectangle.rb
+++ app/models/rectangle.rb
@@ -1,5 +1,9 @@
 class Rectangle < Shape
-   has_one :dimension
+   has_one :dimension, dependent: :destroy
 
    accepts_nested_attributes_for :dimension
+
+   def get_area
+      update(area: self.dimension.width * self.dimension.height)
+   end
 end
